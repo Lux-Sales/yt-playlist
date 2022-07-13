5546581e657851fed4051a48178145245f9f7f6b
lux-sales <sales_l@hotmail.com>
2022-07-13 11:33:22 -0300

    fixing

--- yt-playlist-download.py
+++ yt-playlist-download.py
@@ -3,36 +3,42 @@ import subprocess
 
 from pytube import Playlist, YouTube
 
-def run(pl):
+def run():
+    pl_str = input("Enter playlist url: ")
+    pl = Playlist(pl_str)
     # get parent directory; VERY IMPORTANT!!
     # INCLUDE LAST SLASH AFTER FOLDER NAME
     # e.g. /home/username/Folder/ or C:\Users\Username\Folder\
-    filepath = input("Please enter the filepath of the directory where this script is located:\n")
+    filepath = '/musicas'
     # get linked list of links in the playlist
-    links = pl.parse_links()
+    links = pl.video_urls
     # download each item in the list
+    faileds = []
     for l in links:
-        # converts the link to a YouTube object
-        yt = YouTube(l)
-        # takes first stream; since ffmpeg will convert to mp3 anyway
-        music = yt.streams.first()
-        # gets the filename of the first audio stream
-        default_filename = music.default_filename
-        print("Downloading " + default_filename + "...")
-        # downloads first audio stream
-        music.download()
-        # creates mp3 filename for downloaded file
-        new_filename = default_filename[0:-3] + "mp3"
-        print("Converting to mp3....")
-        # converts mp4 audio to mp3 audio
-        subprocess.run(['ffmpeg', '-i', 
-            os.path.join(filepath, default_filename),
-            os.path.join(filepath, new_filename)
-        ])
-    
+        try:
+            # converts the link to a YouTube object
+            yt = YouTube(l)
+            # takes first stream; since ffmpeg will convert to mp3 anyway
+            music = yt.streams.first()
+            # gets the filename of the first audio stream
+            default_filename = music.default_filename
+            print("Downloading " + default_filename + "...")
+            # downloads first audio stream
+            music.download()
+            # creates mp3 filename for downloaded file
+            new_filename = default_filename[0:-3] + "mp3"
+            print("Converting to mp3....")
+            # converts mp4 audio to mp3 audio
+            subprocess.run(['ffmpeg', '-i', 
+                os.path.join(filepath, default_filename),
+                os.path.join(filepath, new_filename)
+            ])
+        except Exception:
+            faileds.append(new_filename)
+            continue
     print("Download finished.")
-
-if __name__ == "__main__":
-    url = input("Please enter the url of the playlist you wish to download: ")
-    pl = Playlist(url)
-    run(pl)
+    print("Fails:\n")
+    for fail in faileds:
+        print(fail + '\n')
+    
+run()
