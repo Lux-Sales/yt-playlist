a456d815f5ac0ab87c6e1d6d3ad70252b046f47e
lux-sales <sales_l@hotmail.com>
2022-07-19 12:34:38 -0300

    idk

--- yt-playlist-download.py
+++ yt-playlist-download.py
@@ -1,19 +1,17 @@
+import traceback
 import os
-import subprocess
-
 from pytube import Playlist, YouTube
 
 def run():
-    pl_str = input("Enter playlist url: ")
+    pl_str = "https://www.youtube.com/playlist?list=PLv1AVL714MdVNABdWUu9FY19W6jz0eZSC"
     pl = Playlist(pl_str)
     # get parent directory; VERY IMPORTANT!!
     # INCLUDE LAST SLASH AFTER FOLDER NAME
-    # e.g. /home/username/Folder/ or C:\Users\Username\Folder\
-    filepath = '/musicas'
     # get linked list of links in the playlist
     links = pl.video_urls
     # download each item in the list
     faileds = []
+    musics_downloaded = os.listdir('/home/lucas/musicas/yt-playlist/musicas')
     for l in links:
         try:
             # converts the link to a YouTube object
@@ -21,20 +19,17 @@ def run():
             # takes first stream; since ffmpeg will convert to mp3 anyway
             music = yt.streams.first()
             # gets the filename of the first audio stream
-            default_filename = music.default_filename
-            print("Downloading " + default_filename + "...")
-            # downloads first audio stream
-            music.download()
-            # creates mp3 filename for downloaded file
-            new_filename = default_filename[0:-3] + "mp3"
-            print("Converting to mp3....")
-            # converts mp4 audio to mp3 audio
-            subprocess.run(['ffmpeg', '-i', 
-                os.path.join(filepath, default_filename),
-                os.path.join(filepath, new_filename)
-            ])
+            if not music.default_filename.replace(".mp3", ".3gpp") in musics_downloaded:
+                print("Downloading " + music.default_filename + "...")
+                # downloads first audio stream
+                music.download()
+                filename = music.default_filename.replace(".3gpp", ".mp3")
+                os.rename(music.default_filename, filename)
+            else:
+                continue
         except Exception:
-            faileds.append(new_filename)
+            print(traceback.format_exc())
+            faileds.append(music.default_filename)
             continue
     print("Download finished.")
     print("Fails:\n")
